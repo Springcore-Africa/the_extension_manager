8862b4b89326918646811846cdcfaa581148c572
olawhales <ajaditaoreed@gmail.com>
2025-04-01 21:27:07 +0100

    Addition of Spring security code implementation and jwt in the package name SecurityConfiguration and a model name UserPricipal

---
+++ src/main/java/com/oracleous/extention_manager/data/model/Users.java
@@ -0,0 +1,20 @@
+package com.oracleous.extention_manager.data.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Roles userRole;
+}
